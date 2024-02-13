9a6f960592bfded54938aa722142de2163a7fce1
yunus emre Turkoglu <127763579+yunusEmreTurkogl@users.noreply.github.com>
2024-02-13 12:43:34 +0100

    Update ScoutiumV2.py

--- ScoutiumV2.py
+++ ScoutiumV2.py
@@ -1,24 +1,3 @@
-# task_response_id: The set of a scout's evaluations of all players in a team's squad in a match
-# match_id: Id of the relevant match
-# evaluator_id: Id of the evaluator (scout)
-# player_id: Related player's id
-# position_id: The id of the position the player played in that match
-# 1: Goalkeeper
-# 2: Stoper
-# 3: Right back
-# 4: Left bek
-# 5: Defensive midfield
-# 6: Central midfield
-# 7: Right wing
-# 8: Left wing
-# 9: Offensive midfield
-# 10: Striker
-# analysis_id: A set of a scout's attribute evaluations of a player in a match
-# attribute_id: The id of each attribute on which players are evaluated
-# attribute_value: The value a scout gives to a player's attribute (points)
-# potential_label: Label indicating a scout's final decision on a player in a match (target variable)
-
-
 import pandas as pd
 import numpy as np
 import warnings
